d17732a410db4df93577f61b558526df171cf842
AnkushSarkar10 <ankush.sarkar2002@gmail.com>
2023-01-08 03:08:47 -0500

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang='ts'>
+</script>
+
+<template>
+    <div class="container">
+        <h1 class="text-3xl">
+            IRL chess lmaoooo
+        </h1>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
